8f936983893acc73b1af85e94c566a961ee52ce8
Ignatius Christanto <christantoignatius@gmail.com>
2024-07-11 14:10:46 +0700

    config router n axios

---
+++ src/utility/config.js
@@ -0,0 +1,5 @@
+
+export const BASE_URL = "https://pokeapi.co/api/v2";
+export const POKEMON_URL = `${BASE_URL}/pokemon`;
+export const POKEMON_TYPE_URL = `${BASE_URL}/type`;
+export const POKEMON_SPRITES_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon";
